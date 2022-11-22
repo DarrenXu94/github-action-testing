0c8c4f4571b7edf60d1d32efca51a8e22f7739a0
Darren Xu <darrenxu1994@gmail.com>
2022-11-22 20:44:05 +1100

    update target

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const playwright = require("playwright");
 
+const handleConsoleMsg = async (msg) => {
+  try {
+    for (const arg of msg.args()) {
+      const parsed = await arg.jsonValue();
+      if (typeof parsed == "object" && "data" in parsed) {
+        console.log(parsed.data);
+      }
+    }
+  } catch (e) {
+    console.log(e);
+    return;
+  }
+};
+
 async function main() {
   try {
     const browser = await playwright.chromium.launch({
@@ -7,12 +21,22 @@ async function main() {
     });
 
     const page = await browser.newPage();
+    await page.on("console", (msg) => handleConsoleMsg(msg));
 
     await page.goto("https://cf-futsal.dribl.com/ladders/");
     await page.waitForTimeout(5000);
-    let theTitle = await page.title();
+    await page
+      .locator(
+        "#__layout > div > div.nuxt-container.position-relative > section.section.page-filter > div > div.pageFilter.d-none.d-lg-flex.flex-wrap > div:nth-child(2)"
+      )
+      .click();
 
-    console.log("done on page", theTitle);
+    await page
+      .locator(
+        "#__layout > div > div.nuxt-container.position-relative > section.section.page-filter > div > div.pageFilter.d-none.d-lg-flex.flex-wrap > div:nth-child(2) > div.filter-dropdown.shadow.w-100.d-block.position-absolute.top-100.ps.ps--active-x.ps--active-y > ul > li:nth-child(3)"
+      )
+      .click();
+    await page.waitForTimeout(5000);
     await browser.close();
   } catch (e) {
     console.log(e);
